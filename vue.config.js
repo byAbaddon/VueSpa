b4c623e64d12c610b58fd3351cd2fd6e548365c8
byAbbadon <kasiopess@abv.bg>
2019-10-10 22:42:02 +0300

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "transpileDependencies": [
+    "vuetify"
+  ],
+}
+
+//module.exports = { publicPath: 'vue-web-spa', }
+/*
+ git add vue-web-spa && git commit -am "Initial dist subtree commit"
+ git subtree push --prefix vue-web-spa origin gh-pages   
+
+*/
